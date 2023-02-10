9754a72e5729354aed61287e351120d52fb21f94
Parrael <cl201286@g.unicamp.br>
2023-02-10 09:53:11 -0300

    teste

---
+++ Javascript/script.js
@@ -0,0 +1,8 @@
+alert("Hello World!");
+
+function exibirMsg(){
+    alert("Executando funcao");
+}
+
+    document.getElementById("minhaPagina").style.backgroundColor = "black";
+    document.getElementById("titulo").style.color = "red";
